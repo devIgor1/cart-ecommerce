e956a24631211da3decc48333c4908746debe5e2
Igor <igormraesmm@gmail.com>
2023-11-15 08:54:48 -0300

    finish home and cart pages, add fake db

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,29 @@
 export function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">My Cart</h1>
+
+      <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img
+          src="https://i.zst.com.br/thumbs/12/12/10/-928132873.jpg"
+          alt="product-logo"
+          className="w-28"
+        />
+        <strong>Price: $200</strong>
+
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+            -
+          </button>
+          2
+          <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+            +
+          </button>
+        </div>
+        <strong className="float-right">Subtotal: $200</strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: $200</p>
     </div>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,29 @@
+import { BsCartPlus } from "react-icons/bs"
+
 export function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Trending Products
+        </h1>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+          <section className="w-full">
+            <img
+              className="w-full rounded-lg max-h-72 mb-2"
+              src="https://i.zst.com.br/thumbs/12/12/10/-928132873.jpg"
+              alt="product-logo"
+            />
+            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+            <div className="flex gap-3 items-center">
+              <strong className="text-zinc-700/90">$200</strong>
+              <button className="bg-zinc-900 p-1 rounded">
+                <BsCartPlus size={20} color="#fff" />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
