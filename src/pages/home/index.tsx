f48e38f1d46e704c71f73d7757a7da0e081877a7
Igor <igormraesmm@gmail.com>
2023-11-16 18:02:28 -0300

    consuming api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
 import { api } from "../../services/api"
 
+interface ProductProps {
+  id: number
+  title: string
+  description: string
+  price: number
+  cover: string
+}
+
 export function Home() {
-  const [products, setProducts] = useState<[]>([])
+  const [products, setProducts] = useState<ProductProps[]>([])
 
   useEffect(() => {
     async function getProducts() {
       const response = await api.get("/products")
-      console.log(response.data)
+
+      setProducts(response.data)
     }
 
     getProducts()
@@ -21,20 +30,27 @@ export function Home() {
           Trending Products
         </h1>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              className="w-full rounded-lg max-h-72 mb-2"
-              src="https://i.zst.com.br/thumbs/12/12/10/-928132873.jpg"
-              alt="product-logo"
-            />
-            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">$200</strong>
-              <button className="bg-zinc-900 p-1 rounded">
-                <BsCartPlus size={20} color="#fff" />
-              </button>
-            </div>
-          </section>
+          {products.map((product) => (
+            <section key={product.id} className="w-full">
+              <img
+                className="w-full rounded-lg max-h-72 mb-2 border-stone-400 border-2"
+                src={product.cover}
+                alt={product.title}
+              />
+              <p className="font-medium mt-1 mb-2">{product.title}</p>
+              <div className="flex gap-3 items-center">
+                <strong className="text-zinc-700/90">
+                  {product.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </strong>
+                <button className="bg-zinc-900 p-1 rounded">
+                  <BsCartPlus size={20} color="#fff" />
+                </button>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </div>
