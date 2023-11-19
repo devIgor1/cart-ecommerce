703d988859b675dd388279b8169862c7d2e0f1e1
Igor <igormraesmm@gmail.com>
2023-11-19 10:19:24 -0300

    listing store items

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,29 +1,54 @@
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
+
 export function Cart() {
+  const { cart } = useContext(CartContext)
+
   return (
     <div className="w-full max-w-7xl mx-auto">
       <h1 className="font-medium text-2xl text-center my-4">My Cart</h1>
 
-      <section className="flex items-center justify-between border-b-2 border-gray-300">
-        <img
-          src="https://i.zst.com.br/thumbs/12/12/10/-928132873.jpg"
-          alt="product-logo"
-          className="w-28"
-        />
-        <strong>Price: $200</strong>
-
-        <div className="flex items-center justify-center gap-3">
-          <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
-            -
-          </button>
-          2
-          <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
-            +
-          </button>
+      {cart.length === 0 && (
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-medium">Your cart is empty...</p>
+          <Link
+            to="/"
+            className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded"
+          >
+            Access Products
+          </Link>
         </div>
-        <strong className="float-right">Subtotal: $200</strong>
-      </section>
+      )}
 
-      <p className="font-bold mt-4">Total: $200</p>
+      {cart.map((item) => (
+        <section
+          key={item.id}
+          className="flex items-center justify-between border-b-2 border-gray-300"
+        >
+          <img src={item.cover} alt={item.title} className="w-28" />
+          <strong>Price: ${item.price}</strong>
+
+          <div className="flex items-center justify-center gap-3">
+            <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+              -
+            </button>
+            {item.amount}
+            <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+              +
+            </button>
+          </div>
+          <strong className="float-right">
+            Subtotal:{" "}
+            {item.total.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </strong>
+        </section>
+      ))}
+
+      {cart.length !== 0 && <p className="font-bold mt-4">Total: $200</p>}
     </div>
   )
 }
